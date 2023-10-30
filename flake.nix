b29b00043aa5f5a8f48dfe8b0f3c0bb4d9475c5b
Eddio0141 <eddio0141@gmail.com>
2023-10-30 04:00:11 +0000

    added rust pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -27,6 +27,27 @@
                 {
                   # https://devenv.sh/reference/options/
                   languages.rust.enable = true;
+
+                  pre-commit = {
+                    settings.clippy = {
+                      allFeatures = true;
+                      offline = false;
+                    };
+                    hooks = {
+                      rustfmt.enable = true;
+                      clippy.enable = true;
+                      # cargo test
+                      "cargo-test" = {
+                        enable = true;
+                        name = "cargo test";
+                        description = "Run cargo test";
+                        entry = "${self.devShells.${system}.default.config.languages.rust.toolchain.cargo}/bin/cargo test";
+                        fail_fast = true;
+                        pass_filenames = false;
+                        stages = [ "manual" ];
+                      };
+                    };
+                  };
                 }
               ];
             };
