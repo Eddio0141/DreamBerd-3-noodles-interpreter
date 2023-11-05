218dfe19a3db11a1f8c2eec762ef94dac6c43bbc
Eddio0141 <eddio0141@gmail.com>
2023-11-05 22:36:00 +0000

    for now restrict string

--- src/tests/syntax.rs
+++ src/tests/syntax.rs
@@ -18,37 +18,37 @@ fn string_no_quotes_no_space() {
     interpreter_test_output("print hello_world!", "hello_world\n");
 }
 
-#[test]
-fn string_no_quotes_spaces() {
-    interpreter_test_output("print hello world!", "hello world\n");
-}
+// #[test]
+// fn string_no_quotes_spaces() {
+//     interpreter_test_output("print hello world!", "hello world\n");
+// }
 
-#[test]
-fn string_many_quotes() {
-    let code = r#"
-print """"""hello world""""""!
-print '''''hello world'''''!
-"#;
-    interpreter_test_output(code, "hello world\nhello world\n");
-}
+// #[test]
+// fn string_many_quotes() {
+//     let code = r#"
+// print """"""hello world""""""!
+// print '''''hello world'''''!
+// "#;
+//     interpreter_test_output(code, "hello world\nhello world\n");
+// }
 
-#[test]
-fn string_mixed_quotes() {
-    let code = r#"
-print ""''""hello world"""''""!
-print ''""''hello world'''""''!
-"#;
-    interpreter_test_output(code, "hello world\"\nhello world\'\n");
-}
+// #[test]
+// fn string_mixed_quotes() {
+//     let code = r#"
+// print ""''""hello world"""''""!
+// print ''""''hello world'''""''!
+// "#;
+//     interpreter_test_output(code, "hello world\"\nhello world\'\n");
+// }
 
-#[test]
-fn string_single_quote() {
-    let code = r#"
-print 'hello world!
-print hello world"!
-"#;
-    interpreter_test_output(code, "\'hello world!\nhello world\"\n");
-}
+// #[test]
+// fn string_single_quote() {
+//     let code = r#"
+// print 'hello world!
+// print hello world"!
+// "#;
+//     interpreter_test_output(code, "\'hello world!\nhello world\"\n");
+// }
 
 #[test]
 fn string_escape_chars() {
