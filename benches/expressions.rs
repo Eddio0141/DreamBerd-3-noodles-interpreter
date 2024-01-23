87b5f37b0b74213d74fe41847f999500c4ca3402
Eddio0141 <eddio0141@gmail.com>
2024-01-23 04:19:57 +0000

    added obj init

--- benches/expressions.rs
+++ benches/expressions.rs
@@ -10,12 +10,12 @@ pub fn compare_expressions(c: &mut Criterion) {
             interpreter
                 .eval(black_box(
                     r#"
-                print 1 == 2!
-                print 1 ;= 2!
-                print 1 < 2!
-                print 1 <= 2!
-                print 1 > 2!
-                print 1 >= 2!
+print 1 == 2!
+print 1 ;= 2!
+print 1 < 2!
+print 1 <= 2!
+print 1 > 2!
+print 1 >= 2!
             "#,
                 ))
                 .unwrap();
@@ -32,9 +32,9 @@ pub fn compare_expressions_chain(c: &mut Criterion) {
             interpreter
                 .eval(black_box(
                     r#"
-                print 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1!
-                print 1==1 == 1  ==  1   ==   1    ==    1     ==     1      ==      1       ==       1        ==        1         ==         1          ==          1!
-                print 1          ==          1         ==         1        ==        1       ==       1      ==      1     ==     1    ==    1   ==   1  ==  1 == 1!
+print 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1 == 1!
+print 1==1 == 1  ==  1   ==   1    ==    1     ==     1      ==      1       ==       1        ==        1         ==         1          ==          1!
+print 1          ==          1         ==         1        ==        1       ==       1      ==      1     ==     1    ==    1   ==   1  ==  1 == 1!
             "#,
                 ))
                 .unwrap();
@@ -51,8 +51,8 @@ pub fn unary_chain(c: &mut Criterion) {
             interpreter
                 .eval(black_box(
                     r#"
-                print ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;true!
-                print ----------------------------------------------------------------------------------------------------1!
+print ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;true!
+print ----------------------------------------------------------------------------------------------------1!
             "#,
                 ))
                 .unwrap();
@@ -69,8 +69,29 @@ pub fn unary_chain_spaced(c: &mut Criterion) {
             interpreter
                 .eval(black_box(
                     r#"
-                print ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; true!
-                print - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 1!
+print ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; true!
+print - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 1!
+            "#,
+                ))
+                .unwrap();
+        });
+    });
+}
+
+pub fn obj_initialiser(c: &mut Criterion) {
+    let mut stdout = std::io::sink();
+    let interpreter = InterpreterBuilder::with_stdout(&mut stdout).build();
+
+    c.bench_function("obj_initialiser", |b| {
+        b.iter(|| {
+            interpreter
+                .eval(black_box(
+                    r#"
+var foo = { 
+    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9,
+    j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18,
+    s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+}!
             "#,
                 ))
                 .unwrap();
