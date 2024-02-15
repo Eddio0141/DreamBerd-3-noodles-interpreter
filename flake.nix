a464b23f2e046f3f7fe832d0f46c316a94404aed
Eddio0141 <eddio0141@gmail.com>
2024-02-15 12:38:06 +0000

    swap devenv to a non shit one

--- flake.nix
+++ flake.nix
@@ -1,65 +1,39 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    fenix.url = "github:nix-community/fenix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs = { flake-parts, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { self', inputs', system, pkgs, ... }:
+      let
+        fenix = inputs'.fenix.packages.complete.withComponents [
+          "rustc"
+          "cargo"
+          "rustfmt"
+          "rust-analyzer"
+          "clippy"
+          "rust-src"
+        ];
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  languages.rust = {
-                    enable = true;
-                    channel = "nightly";
-                  };
-
-                  packages = with pkgs; [
-                    gnuplot_qt
-                    openssl
-                  ];
-
-                  pre-commit = {
-                    # settings.clippy = {
-                    #   allFeatures = true;
-                    #   offline = false;
-                    # };
-                    hooks = {
-                      rustfmt.enable = true;
-                      # clippy.enable = true;
-                      # cargo test
-                      # "cargo-test" = {
-                      #   enable = true;
-                      #   name = "cargo test";
-                      #   description = "Run cargo test";
-                      #   entry = "${self.devShells.${system}.default.config.languages.rust.toolchain.cargo}/bin/cargo test";
-                      #   fail_fast = true;
-                      #   pass_filenames = false;
-                      #   stages = [ "manual" ];
-                      # };
-                    };
-                  };
-                }
-              ];
-            };
-          });
+        rust-doc = pkgs.writeShellApplication {
+          name = "rust-doc";
+          text = ''
+            xdg-open "${inputs'.fenix.packages.complete.rust-docs}/share/doc/rust/html/index.html"
+          '';
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fenix rust-doc
+          ];
+        };
+      };
     };
 }
