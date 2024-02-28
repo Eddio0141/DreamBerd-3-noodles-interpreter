9cfd9b4b9f5a27a714555ac4748db4db19908826
Eddio0141 <eddio0141@gmail.com>
2024-02-28 04:44:47 +0000

    basic parsing

--- src/interpreter/evaluators/class.rs
+++ src/interpreter/evaluators/class.rs
@@ -1,8 +1,32 @@
-use crate::impl_parser;
+use nom::{
+    branch::alt, bytes::complete::tag, character::complete::char, combinator::fail, sequence::tuple,
+};
+
+use crate::{
+    parsers::{identifier, types::Position},
+    runtime, Interpreter,
+};
+
+use super::{parsers::AstParseResult, EvalArgs};
 
 #[derive(Debug, Default)]
-pub struct Classes {}
+struct Class {
+    name: String,
+}
 
-struct Class {}
+impl Class {
+    pub fn parse(input: Position<Interpreter>) -> AstParseResult<Self> {
+        let class = alt((tag("className"), tag("class")));
+        let name = identifier(fail::<_, (), _>);
+        let open = char('{');
+        let close = char('}');
 
-impl_parser!(Class, input, { todo!() }, self, args, { todo!() }, ());
+        let (input, (_, name, _, _)) = tuple((class, name, open, close))(input)?;
+
+        todo!()
+    }
+
+    pub fn eval(args: EvalArgs) -> Result<(), runtime::Error> {
+        todo!()
+    }
+}
