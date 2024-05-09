5464f374d2c3cbdb4d92489e2a71ddca577dd555
Eddio0141 <eddio0141@gmail.com>
2024-05-09 05:52:31 +0100

    print fixed

--- src/interpreter/runtime/stdlib/stdio.rs
+++ src/interpreter/runtime/stdlib/stdio.rs
@@ -7,15 +7,11 @@ use crate::{
 };
 
 pub fn print(_interpreter: &Interpreter, args: Vec<Wrapper<Cow<Value>>>) -> Result<Value, Error> {
-    let mut stdout = std::io::stdout();
+    let mut stdout = std::io::stdout().lock();
     for arg in args {
-        stdout
-            .write_fmt(format_args!("{}", arg.as_ref()))
+        writeln!(stdout, "{}", arg.as_ref())
             .map_err(|err| Error::RuntimeException(err.to_string()))?;
     }
-    stdout
-        .flush()
-        .map_err(|err| Error::RuntimeException(err.to_string()))?;
 
     Ok(Value::Undefined)
 }
