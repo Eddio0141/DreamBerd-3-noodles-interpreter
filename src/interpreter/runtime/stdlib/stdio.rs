1e42e114738e5f281925791f51c3c0f72659b59e
Eddio0141 <eddio0141@gmail.com>
2024-05-09 06:20:11 +0100

    input API

--- src/interpreter/runtime/stdlib/stdio.rs
+++ src/interpreter/runtime/stdlib/stdio.rs
@@ -15,3 +15,20 @@ pub fn print(_interpreter: &Interpreter, args: Vec<Wrapper<Cow<Value>>>) -> Resu
 
     Ok(Value::Undefined)
 }
+
+pub fn input(_interpreter: &Interpreter, args: Vec<Wrapper<Cow<Value>>>) -> Result<Value, Error> {
+    let mut stdout = std::io::stdout();
+    let stdin = std::io::stdin();
+    for arg in args {
+        write!(stdout, "{}", arg.as_ref())
+            .map_err(|err| Error::RuntimeException(err.to_string()))?;
+    }
+    stdout
+        .flush()
+        .map_err(|err| Error::RuntimeException(err.to_string()))?;
+    let mut input = String::new();
+    stdin
+        .read_line(&mut input)
+        .map_err(|err| Error::RuntimeException(err.to_string()))?;
+    Ok(Value::String(input.trim().to_string()))
+}
